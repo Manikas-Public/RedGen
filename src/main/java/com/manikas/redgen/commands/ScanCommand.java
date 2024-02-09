deacb84d71c11579b2f5fd9fdae72369b47ca42c
manikas <dziugas.storpirstis@gmail.com>
2024-02-09 23:55:48 +0200

    Initial scan loop + array chat feedback

--- src/main/java/com/manikas/redgen/commands/ScanCommand.java
+++ src/main/java/com/manikas/redgen/commands/ScanCommand.java
@@ -12,7 +12,11 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.arguments.coordinates.BlockPosArgument;
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.event.entity.EntityJoinLevelEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -28,7 +32,7 @@ public class ScanCommand {
     }
 
     public ScanCommand(CommandDispatcher<CommandSourceStack> dispatcher) {
-        dispatcher.register(Commands.literal("scan").then(Commands.argument("bounds_1", BlockPosArgument.blockPos()).then(Commands.argument("bounds2", BlockPosArgument.blockPos()).then(Commands.argument("scan_name", StringArgumentType.string()).executes(this::execute)))));
+        dispatcher.register(Commands.literal("scan").then(Commands.argument("bounds1", BlockPosArgument.blockPos()).then(Commands.argument("bounds2", BlockPosArgument.blockPos()).then(Commands.argument("scan_name", StringArgumentType.string()).executes(this::execute)))));
     }
 
     private int execute(CommandContext<CommandSourceStack> context) {
@@ -37,8 +41,35 @@ public class ScanCommand {
             String pointerName = useablePointerEntity.getName().toString();
 
             Minecraft.getInstance().player.sendSystemMessage(Component.literal(StringArgumentType.getString(context, "scan_name")));
-            Minecraft.getInstance().player.sendSystemMessage(Component.literal((BlockPosArgument.getBlockPos(context, "bounds")).toString()));
+            Minecraft.getInstance().player.sendSystemMessage(Component.literal((BlockPosArgument.getBlockPos(context, "bounds1")).toString()));
+            Minecraft.getInstance().player.sendSystemMessage(Component.literal((BlockPosArgument.getBlockPos(context, "bounds2")).toString()));
 
+            int repeatsx = Math.abs(BlockPosArgument.getBlockPos(context, "bounds1").getX() - BlockPosArgument.getBlockPos(context, "bounds2").getX());
+            int repeatsy = Math.abs(BlockPosArgument.getBlockPos(context, "bounds1").getY() - BlockPosArgument.getBlockPos(context, "bounds2").getY());
+            int repeatsz = Math.abs(BlockPosArgument.getBlockPos(context, "bounds1").getZ() - BlockPosArgument.getBlockPos(context, "bounds2").getZ());
+
+            int indexX = 0;
+            int indexY = 0;
+//            int indexZ = 0;
+
+            Block[] currentScan = new Block[repeatsx*repeatsy*repeatsz];
+
+            for (int z = 0; z < repeatsz; z++) {
+                for (int y = 0; y < repeatsy; y++) {
+                    useablePointerEntity.setPos(BlockPosArgument.getBlockPos(context, "bounds1").getX(), BlockPosArgument.getBlockPos(context, "bounds1").getY() + Math.floorDiv(indexX,repeatsy), useablePointerEntity.getZ() + indexY);
+                    for (int x = indexX * repeatsx; x < repeatsx; x++) {
+                        if (context.getSource().getLevel().getBlockState(new BlockPos(useablePointerEntity.getBlockX(), useablePointerEntity.getBlockY(), useablePointerEntity.getBlockZ())).getBlock() != Blocks.AIR) {
+                            currentScan[x] = context.getSource().getLevel().getBlockState(new BlockPos(useablePointerEntity.getBlockX(), useablePointerEntity.getBlockY(), useablePointerEntity.getBlockZ())).getBlock();
+                        } else {
+                            currentScan[x] = null;
+                        }
+                    }
+                    indexX++;
+                }
+                indexY++;
+            }
+
+            Minecraft.getInstance().player.sendSystemMessage(Component.literal(currentScan[0].toString()));
             context.getSource().sendSuccess(() -> Component.literal("Current entity " + pointerName + " at " + useablePointerEntity.getBlockX() + " " + useablePointerEntity.getBlockY() + " " + useablePointerEntity.getBlockZ()), true);
             return 1;
         }else {
